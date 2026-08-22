89f1631c01b6ff8ba2189adc1f1173ed1107db39
Rafael Gomes <rafarfl2010@gmail.com>
2026-08-22 00:01:24 -0300

    feat(component): using Header + breakpoints

--- blog/app/components/Header/index.tsx
+++ blog/app/components/Header/index.tsx
@@ -1,13 +1,17 @@
-'use client';
 import clsx from "clsx";
 
 export default function Header() {
-  console.log('header component');
   return(
-    <h1 className={clsx(
-        'text-6x1 font-bold text-blue-500',
-        'hover:text-blue-50 hover:bg-blue-500',
-        'transition duration-1000'
-      )} onClick={() => alert('ok')}>Home</h1>
+    <header>
+      <h1 className={clsx(
+        'text-4xl/normal font-extrabold py-8',
+        'sm:text-5xl/normal sm:py-10',
+        'md:text-6xl/normal md:py-11',
+        'lg:text-7x1/normal lg:py-12'
+      )}
+      >
+        <a href="#">The Blog</a>
+      </h1>
+    </header>
   )
 }
--- blog/app/page.tsx
+++ blog/app/page.tsx
@@ -1,19 +1,19 @@
 import { Suspense } from "react";
 import { SpinLoader } from "./components/SpinLoader";
 import { PostsList } from "./components/PostsList";
+import { Container } from "./components/Container";
+import Header from "./components/Header";
 
 export default async function Home() {
 
 
   return (
-    <div className="text-slate-900 bg-slate-100 min-h-screen dark:bg-slate-900 dark:text-slate-100">
-      <header>
-        <h1 className='text-6x1 font-bold text-center py-8'>Aqui é o header</h1>
-      </header>
-      <Suspense fallback={<SpinLoader className="h-10 w-10" />}>
-        <PostsList />
-      </Suspense>
-      <footer><h1 className='text-6x1 font-bold text-center py-8'>footer</h1></footer>
-    </div>
+    <Container>
+        <Header />
+        <Suspense fallback={<SpinLoader className="h-10 w-10" />}>
+          <PostsList />
+        </Suspense>
+        <footer><h1 className='text-6x1 font-bold text-center py-8'>footer</h1></footer>
+    </Container>
   );
 }
