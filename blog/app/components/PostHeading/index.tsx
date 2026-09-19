61d0b1c0aed4398950f90316ff1ab0792fe8eac4
Rafael Gomes <rafarfl2010@gmail.com>
2026-09-19 19:55:10 -0300

    feat(style): creating PostList responsive grid

--- blog/app/components/PostHeading/index.tsx
+++ blog/app/components/PostHeading/index.tsx
@@ -10,15 +10,13 @@ type PostHeadingProps = {
 export function PostHeading({ children, url, as: Tag = 'h2' }: PostHeadingProps) {
 
   const headingClassesMap = {
-    h1: 'text-2xl/tight font-extrabold sm:text-4xl',
-    h2: 'text-2xl/tight font-extrabold sm:text-3xl',
+    h1: 'text-2xl/tight font-extrabold sm:text-4xl font-extrabold',
+    h2: 'text-2xl/tight font-bold',
   }
 
-  const commonClasses = 'font-extrabold';
-
   return (
-    <Tag className={clsx(headingClassesMap[Tag], commonClasses)}>
-      <Link href={url}>{children}</Link>
+    <Tag className={clsx(headingClassesMap[Tag])}>
+      <Link className="hover:text-slate-600 transition" href={url}>{children}</Link>
     </Tag>
   )
 }
