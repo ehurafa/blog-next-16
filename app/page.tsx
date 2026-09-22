dcce312126bc9e5ccd1a314922706484ef0b23bc
Rafael Gomes <rafarfl2010@gmail.com>
2026-09-22 00:46:49 -0300

    feat(config): moving project folder

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,19 @@
 import { Suspense } from "react";
 import { SpinLoader } from "./components/SpinLoader";
 import { PostsList } from "./components/PostsList";
-import { Container } from "./components/Container";
-import Header from "./components/Header";
 import { PostFeatured } from "./components/PostFeatured";
 
 export default async function Home() {
 
   return (
-    <Container>
-        <Header />
-
-        <Suspense fallback={<SpinLoader className="h-10 w-10" />}>
+    <>
+      <Suspense fallback={<SpinLoader className="h-10 w-10" />}>
           <PostFeatured />
         </Suspense>
 
         <Suspense fallback={<SpinLoader className="h-10 w-10" />}>
           <PostsList />
         </Suspense>
-        <footer><h1 className='text-6x1 font-bold text-center py-8'>footer</h1></footer>
-    </Container>
+    </>
   );
 }
