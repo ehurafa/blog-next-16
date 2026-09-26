3430222bfbe4607affd383b52aba5bace90b0d00
Rafael Gomes <rafarfl2010@gmail.com>
2026-09-25 21:40:26 -0300

    feat(content):  markdown tp HTML renderization

--- app/components/SinglePost/index.tsx
+++ app/components/SinglePost/index.tsx
@@ -4,6 +4,7 @@ import { SpinLoader } from "../SpinLoader";
 import Image from "next/image";
 import { PostHeading } from "../PostHeading";
 import { PostDate } from "../PostDate";
+import { SafeMarkdown } from "../SafeMarkdown";
 
 type SinglePostProps = {
   slug: string;
@@ -32,7 +33,7 @@ export async function SinglePost({ slug }: SinglePostProps) {
 
         <p className="text-xl mb-4 text-slate-600">{post.excerpt}</p>
 
-        <div>{post.content}</div>
+        <SafeMarkdown markdown={post.content} />
       </article>
     </Suspense>
   )
